adeb2fa1fbd460dc1d644933495a980ec8106a01
Md Mahmudunnobe <mahmud.nobe@edu.minerva.edu>
2022-11-18 02:03:24 -0500

    change the sliding speed for different screensize

---
+++ script.js
@@ -0,0 +1,16 @@
+//var w = window.innerWidth;
+//var h = window.innerHeight;
+//console.log(w, h);
+
+
+window.addEventListener('resize', function() {
+    //console.log(window.innerWidth);
+    carousel = d3.select('.carousel');
+    if (window.innerWidth < 575) {
+        carousel.attr("data-mdb-interval", 20000)
+    }
+    else {
+        carousel.attr("data-mdb-interval", 10000)
+    }
+    //console.log(carousel.attr('data-mdb-interval'));
+})
